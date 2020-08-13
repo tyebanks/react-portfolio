aa8cb3e0bb359a87cf47ab047758e644cff12fc6
tyebanks <ebankst07@gmail.com>
2020-08-13 05:05:23 +0100

    sticky nav bar added

--- src/components/Project.js
+++ src/components/Project.js
@@ -4,8 +4,13 @@ const Project = (props) => {
     return (
         <div>
             <h1 className="title is-1">{props.heading}</h1> 
-            <h2 className="title is-4">{props.title}</h2>
-            <h3 className="title is-5">{props.subtitle}</h3>
+            <div className="card">
+                 <div className="card-content">                
+                   <h2 className="subtitle is-4">{props.title}</h2>
+                   <h3 className="subtitle is-5">{props.subtitle}</h3>
+                   <p>{props.description}</p>
+                   </div>
+            </div>
         </div>
     )
 }
