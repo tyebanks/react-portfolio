934058afae223686d55dcb8112b582afb5a803ff
tyebanks <ebankst07@gmail.com>
2020-08-13 14:50:40 +0100

    cards customized and centered

--- src/components/Project.js
+++ src/components/Project.js
@@ -3,30 +3,25 @@ import React from 'react';
 const Project = (props) => {
     return (
         <div>
-           
-            
-                 <div class="column">
-                    <div className="card project_image">
-                        <div className="card-content">
-                            <div className="media">
-                              <div className="media-content">
-                                     <h2 className="title is-4">{props.title}</h2>
-                                     <h3 className="title is-6">{props.subtitle1}</h3>
-                              </div>                          
-                            </div>
-                            <div className="card-image">
-                                <figure className="image is-4by3">{props.image}</figure>
-                            </div>
-                        
-                                        
-                            <div className="card-footer">
-                              {/* <span><p className="card-footer-item">{props.description}</p></span> */}
-                              <h3 className="card-footer-item subtitle is-5">{props.subtitle2}</h3>
-                            </div>
-                        </div>
+           <div class="column mx-5 mb-5 card_width">
+              <div className="card project_image">
+                  <div className="card-content">
+                      <div className="media">
+                          <div className="media-content">
+                               <h2 className="card_title">{props.title}</h2>
+                          </div>                        
+                      </div>
+                      <div className="card-image">
+                          <figure className="image is-4by3">{props.image}</figure>
+                      </div>
                     </div>
-                 </div>
-            
+                                        
+                    <div className="card-footer">
+                         <span className="card-footer-item"><h3 className="title is-6 has-text-primary-dark">{props.subtitle1}</h3></span> 
+                         <span className="card-footer-item"><h3 className="title is-6">{props.subtitle2}</h3></span> 
+                    </div>       
+              </div>
+            </div>    
         </div>
     )
 }
--- src/sections/Projects.js
+++ src/sections/Projects.js
@@ -10,12 +10,11 @@ class Projects extends Component {
  let heading =  "My Projects";
         return (
             <div className="hero is-light is-bold Projects-Container">
-               <div><h1 className="has-text-centered title is-1 mt-6 mb-6">{heading}</h1></div>
-              <div className="columns">
-                <Project title="Web Design: 2" subtitle1="GatsbyJS + Bootstrap" subtitle2="Outlandish.ky" image={<img src={project1} alt="outlandish-project"/>}/>
-                <Project title= "Web Development: 2" subtitle1="JavaScript" subtitle2="Hero Game" image={<img src={project2} alt="hero-game" />}/>  
-                <Project title= "Web Development: 1" subtitle1="HTML5 + CSS3" subtitle2="Web Dev News Site" image={<img src={project3} alt="news-site" />}/> 
-                  
+               <div><h1 className="has-text-centered title is-1">{heading}</h1></div>
+              <div className="columns is-centered pb-0">
+                <Project title="Web Design: 2" subtitle1="Outlandish" subtitle2="GatsbyJS" image={<img src={project1} alt="outlandish-project"/>}/>
+                <Project title= "Web Development: 2" subtitle1="Hero Game" subtitle2="JavaScript" image={<img src={project2} alt="hero-game" />}/>  
+                <Project title= "Web Development: 1" subtitle1="Web News Site" subtitle2="HTML5 + CSS3" image={<img src={project3} alt="news-site" />}/>  
               </div>        
             </div>
         )
