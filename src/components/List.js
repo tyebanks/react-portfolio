51da637e3011974a2ba3ec3f24fac79c950a52b4
tyebanks <ebankst07@gmail.com>
2020-08-13 17:25:40 +0100

    education elements added

--- src/components/List.js
+++ src/components/List.js
@@ -7,9 +7,7 @@ const List = (props) => {
                  <header className="card-header mb-3">
                        <h2 className="card-header-title">{props.title}</h2>
                  </header>     
-                 <div className="card-image">
-                      <figure className="image is-128x128">{props.image}</figure>
-                 </div>                   
+                 <div className={props.classes}></div>                   
                  <div className="card-content">
                       <p className="">{props.text1}</p>
                       <p className="">{props.text2}</p>
