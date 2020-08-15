1ee6eff5b1e47d57f06ac7f745f9cc9d94464c83
tyebanks <ebankst07@gmail.com>
2020-08-15 18:14:26 +0100

    changing column for skills into horizontal columns

--- src/sections/Skills.js
+++ src/sections/Skills.js
@@ -5,7 +5,7 @@ import List from '../components/List';
 class Skills extends Component {
  render() {
  let heading =  "TECHNICAL SKILLS";
- const subheading = "Web Programming & Other Tools";
+ const subheading = "Web Programming & Other Tools:";
     return (
         // // Skills-Container   <-- old classname
         <div className="section hero is-light"> 
@@ -17,11 +17,11 @@ class Skills extends Component {
             <div className="custom-subheading is-size-5-mobile column is-four-fifths-mobile"> 
                  {subheading}
             </div>   
-            <div className="level-item">              
+            <div className="level-item is-mobile">              
                   <div className="column is-four-fifths-mobile">           
                      <List text1="HTML5" text2="CSS3" text3="JavaScript" text4="PHP" text5="ReactJS" text6="GatsbyJS"/>
                      <List text1="Java/Eclipse" text2="WordPress" text3="Joomla" text4="GitHub" text5="Atom" text6="VS Code"/>
-                     <List text1="Adobe Illustrator" text2="Adobe PhotoShop" text3="Figma" text4="inVision" text5="Discord" text6="Slack"/>
+                     {/* <List text1="Adobe Illustrator" text2="Adobe PhotoShop" text3="Figma" text4="inVision" text5="Discord" text6="Slack"/> */}
             </div>
             </div>
         </div>
