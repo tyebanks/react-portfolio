ba9486efc90772d5083ad45c6891dbfd6e1db650
tyebanks <ebankst07@gmail.com>
2020-08-15 05:20:35 +0100

    adding padding to fix image being under the nav

--- src/components/MainImage.js
+++ src/components/MainImage.js
@@ -4,8 +4,8 @@ import myImg from '../imgs/portfolioImg.jpg';
 class MainImage extends Component {
     render() {   
       return (
-        <div className="column is-full pt-5 mainImage">
-          <figure className="image "><img src={myImg} className="myImg is-rounded" alt="Ty Ebanks" /></figure> 
+        <div className="column is-full  mainImage">
+          <figure className="image mt-5 "><img src={myImg} className="myImg is-rounded" alt="Ty Ebanks" /></figure> 
         </div>
       );
     }
