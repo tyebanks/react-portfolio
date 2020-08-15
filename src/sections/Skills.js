4e750f3dd008b3c5b7cf55b9ade70077000f127c
tyebanks <ebankst07@gmail.com>
2020-08-15 06:11:40 +0100

    added is-touch for tablet responsiveness

--- src/sections/Skills.js
+++ src/sections/Skills.js
@@ -11,13 +11,13 @@ class Skills extends Component {
             <div className="columns is-centered mt-6 py-5">
                 <h1 id="skills" className="has-text-centered title is-1">{heading}</h1>
             </div>                  
-            <header className="is-mobile is-tablet">     
-            <div className="has-text-centered"><List title="Web Programming, Design & Collaborative Tools"/></div>
-            <div className="level-item has-text-centered">
+            <header className="level is-touch">     
+               <div className="has-text-centered"><List title="Web Programming, Design & Collaborative Tools"/></div>
+                  <div className="level-item has-text-centered">
            
-               <List text1="HTML5" text2="CSS3" text3="JavaScript" text4="PHP" text5="ReactJS" text6="GatsbyJS"/>
-                <List text1="Java/Eclipse" text2="WordPress" text3="Joomla" text4="GitHub" text5="Atom" text6="VS Code"/>
-                <List text1="Adobe Illustrator" text2="Adobe PhotoShop" text3="Figma" text4="inVision" text5="Discord" text6="Slack"/>
+                     <List text1="HTML5" text2="CSS3" text3="JavaScript" text4="PHP" text5="ReactJS" text6="GatsbyJS"/>
+                     <List text1="Java/Eclipse" text2="WordPress" text3="Joomla" text4="GitHub" text5="Atom" text6="VS Code"/>
+                     <List text1="Adobe Illustrator" text2="Adobe PhotoShop" text3="Figma" text4="inVision" text5="Discord" text6="Slack"/>
             </div>
             </header>
         </div>
