9309133ebd1f7f8c484abdc0ce4d282f82e58e10
tyebanks <ebankst07@gmail.com>
2020-08-15 17:24:24 +0100

    Technical Skills Mobile Responsive

--- src/components/List.js
+++ src/components/List.js
@@ -3,15 +3,14 @@ import React from 'react';
 const List = (props) => {
     return (
         <div>
-           <div class="column mx-6 mb-5 ">
-                 <h2 className="title is-3">{props.title}</h2>
-                 
-                  <p className="">{props.text1}</p>
-                  <p className="">{props.text2}</p>
-                  <p className="">{props.text3}</p>
-                  <p className="">{props.text4}</p>
-                  <p className="">{props.text5}</p>
-                  <p className="">{props.text6}</p>                           
+           <div class="column mb-5 ">
+                 {/* <p className="is-one-quarter">{props.title}</p> */}
+                  <p>{props.text1}</p>
+                  <p>{props.text2}</p>
+                  <p>{props.text3}</p>
+                  <p>{props.text4}</p>
+                  <p>{props.text5}</p>
+                  <p>{props.text6}</p>                           
             </div>    
         </div>
     )
--- src/sections/Skills.js
+++ src/sections/Skills.js
@@ -4,22 +4,27 @@ import List from '../components/List';
 
 class Skills extends Component {
  render() {
- let heading =  "Technical Skills";
+ let heading =  "TECHNICAL SKILLS";
+ const subheading = "Web Programming & Other Tools";
     return (
-        <div className="hero is-light is-bold Skills-Container">
-        <div className= "has-text-centered-touch">
-            <div className="columns is-centered mt-6 py-5">
-                <h1 id="skills" className="has-text-centered title is-1">{heading}</h1>
-            </div>                  
-            <header className="level is-touch">     
-               <div className="has-text-centered"><List title="Web Programming, Design & Collaborative Tools"/></div>
-                  <div className="level-item has-text-centered">
-           
+        // // Skills-Container   <-- old classname
+        <div className="section hero is-light"> 
+        <div className="container is-fluid">
+        <div className= "columns has-text-left-touch">
+            <div id="skills" className="level-item is-mobile is-full-mobile title is-size-2-mobile is-size-1-tablet">
+                {heading}
+            </div>       
+            <div className="custom-subheading is-size-5-mobile pl-6 column is-three-fifths-mobile"> 
+                 {subheading}
+            </div>   
+            <div className="level-item">              
+                  <div className="column is-four-fifths-mobile">           
                      <List text1="HTML5" text2="CSS3" text3="JavaScript" text4="PHP" text5="ReactJS" text6="GatsbyJS"/>
                      <List text1="Java/Eclipse" text2="WordPress" text3="Joomla" text4="GitHub" text5="Atom" text6="VS Code"/>
                      <List text1="Adobe Illustrator" text2="Adobe PhotoShop" text3="Figma" text4="inVision" text5="Discord" text6="Slack"/>
             </div>
-            </header>
+            </div>
+        </div>
         </div>
     </div>
   );
