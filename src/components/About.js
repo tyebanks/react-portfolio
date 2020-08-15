0b38ae81b4c8100442365aa526a11bee98905fba
tyebanks <ebankst07@gmail.com>
2020-08-15 05:08:06 +0100

    centering paragraph on landing page

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class About extends Component {
   render() {   
       return (
-           <div className="container mt-1 pb-2 columns about-Container has-text-left-mobile has-text-left-tablet">
+           <div className="container mt-1 pb-2 columns about-Container has-text-left-mobile has-text-centered-tablet">
               <div className="column is-two-thirds about-Text">
                   <h1 className="is-size-2-touch is-size-1-desktop">OH, HELLO THERE.</h1>
                   <p className="has-text-black is-size-5-mobile is-size-4-tablet">I’m Ty! I love designing websites. I currently spend
